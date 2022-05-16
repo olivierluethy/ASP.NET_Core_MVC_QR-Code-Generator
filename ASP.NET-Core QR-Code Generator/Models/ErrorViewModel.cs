95f4192cb19417631f33e61b1a7370757f2a827c
Olivier_Luethy <olivier@kauz.ch>
2022-05-16 10:39:04 +0200

    Projektdateien hinzufügen.

---
+++ ASP.NET-Core QR-Code Generator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Core_QR_Code_Generator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
